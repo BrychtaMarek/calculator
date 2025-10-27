27a9b2f3cc624ef19010247caa4a883515216781
thecifx <brychta.ma@gmail.com>
2025-10-27 22:30:40 +0100

    Fix adding multi-digit numbers

--- calculator.js
+++ calculator.js
@@ -1,23 +1,26 @@
-function add(a, b){
-    return a + b;
+function add(a, b) {
+    return Number(a) + Number(b);
 }
 
-function subtract( a, b){
-    return a - b;
+function subtract(a, b) {
+    return Number(a) - Number(b);
 }
 
-function multiply(a, b){
-    return a * b
+function multiply(a, b) {
+    return Number(a) * Number(b)
 }
 
-function divide(a, b){
+function divide(a, b) {
     if (b) {
-        return a / b
-    } else return 'Zero division'
-    
+        return Number(a) / Number(a)
+    } else if (Number(b) === 0) {
+        return 'Zero division'
+    }
+    else return 'NaN'
+
 }
 
-function operate(a, b, operator){
+function operate(a, b, operator) {
     let equals;
     switch (operator) {
         case '+':
@@ -35,10 +38,13 @@ function operate(a, b, operator){
         default:
             break;
     }
-    return round(equals, 4)
+
+    if (typeof (equals) === Number) {
+        return round(equals, 4);
+    } else return equals;
 }
 
-function round(value, decimals){
+function round(value, decimals) {
     const multiplier = Math.pow(10, decimals)
     console.log(multiplier)
     return Math.round(value * multiplier) / multiplier
@@ -48,32 +54,42 @@ let num1;
 let operator;
 let num2;
 
-const display = document.querySelector('#display')
+const btnDisplay = document.querySelector('#display')
 const digitsAndOperators = document.querySelectorAll('.digit, .operator')
+let isSecondNumberStarted = false;
 
 digitsAndOperators.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         const isDigit = e.target.classList.contains('digit');
         const isOperator = e.target.classList.contains('operator');
         const value = e.target.textContent;
+        let shouldResetDisplay = false;
 
-        if (num1 && num2){
-            //debugger
+        if (num1 && num2 && isOperator) {
             const result = operate(num1, num2, operator);
-            display.value = result;
+            btnDisplay.value = result;
             num1 = result;
             num2 = undefined;
         }
-        else if (!num1 && isDigit) {
-            num1 = Number(value)
-        } else if ( num1 && isDigit){
-            num2 = Number(value)
+        else if (!isSecondNumberStarted && isDigit) {
+            num1 = num1 === undefined ? value : num1 + value;
+        } else if (num1 && isDigit && !num2) {
+            num2 = value;
+            btnDisplay.value = '';
+            shouldResetDisplay = true;
+        } else if (num1 && isDigit && num2) {
+            num2 += value;
         } else if (isOperator) {
+            isSecondNumberStarted = true;
             operator = value
-        } 
+        }
 
-        if (isDigit){
-            display.value = e.target.textContent;
+        if (isDigit) {
+            if (isSecondNumberStarted && shouldResetDisplay){
+                btnDisplay.value = e.target.textContent;
+            } else {
+                btnDisplay.value += e.target.textContent;
+            }
         }
     })
 })
@@ -81,20 +97,23 @@ digitsAndOperators.forEach((btn) => {
 const btnEquals = document.querySelector('#btnEquals')
 
 btnEquals.addEventListener('click', () => {
-    if (num1 && num2 && operator) {
-     const result = operate(num1, num2, operator);
-     display.value = result;
-     num1 = undefined;
-     num2 = undefined;
-     operator = undefined;
+    if (num1 && (num2 || num2 === 0) && operator) {
+        const result = operate(num1, num2, operator);
+        btnDisplay.value = result;
+        num1 = result;
+        num2 = undefined;
+        operator = undefined;
     }
 })
 
+function reset() {
+    btnDisplay.value = '';
+    num1 = undefined;
+    num2 = undefined;
+    operator = undefined;
+    isSecondNumberStarted = false;
+}
+
 const btnClear = document.querySelector('#reset')
 
-btnClear.addEventListener('click', (e) => {
-    display.value = '';
-     num1 = undefined;
-     num2 = undefined;
-     operator = undefined;
-})
+btnClear.addEventListener('click', () => reset())
